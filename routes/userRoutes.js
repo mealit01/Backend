12e1f8f5319600381cae43100b95421c288bc7ed
lamasalah32 <lamasalah32@gmail.com>
2023-06-16 05:21:29 +0300

    add routes folder

---
+++ routes/userRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const userController = require('./../controllers/userController');
+const router = express.Router();
+const authController = require('./../controllers/authController');
+
+router.post(`/signup`, authController.signup);
+
+router.post(`/login`, authController.login);
+router.post(`/forgetPassword`, authController.forgetPass);
+router.patch(`/resetPassword/:token`, authController.resetPass);
+router.patch(
+  `/updatePassword`,
+  authController.protect,
+  authController.updatePass
+);
+router.patch(`/updateInfo`, authController.protect, userController.update);
+router.delete(`/delete`, authController.protect, userController.delete);
+
+router.route(`/`).get(authController.protect, userController.getAllUsers);
+
+module.exports = router;
