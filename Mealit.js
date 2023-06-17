651fe2b3951db60f74ff64e8ecd387e9a561a69a
lamasalah32 <lamasalah32@gmail.com>
2023-06-18 02:03:11 +0300

    update Mealit.js

--- Mealit.js
+++ Mealit.js
@@ -10,11 +10,11 @@ const morgan = require('morgan');                       // HTTP request logger m
 const AppError = require('./utils/appError');            // Custom error handling utility
 const globalErrorHandler = require('./controllers/errorController'); // Global error handling middleware
 const userRouter = require('./routes/userRoutes.js');       // User route handler
-const recipeRouter = require('./routes/recipeRoutes');   // Recipe route handler
+//const recipeRouter = require('./routes/recipeRoutes');   // Recipe route handler
 const cors = require("cors");
 const Mealit = express();                               // Create an Express application instance
 
-//Mealit.use(cors());
+Mealit.use(cors());
 
 Mealit.use(express.json());                             // Middleware to parse incoming requests with JSON payloads
 
@@ -58,7 +58,7 @@ Mealit.use((req, res, next) => {
 Mealit.use(`/api/user`, userRouter);
 
 // Route handler for recipe routes
-Mealit.use(`/api/recipes`, recipeRouter);
+//Mealit.use(`/api/recipes`, recipeRouter);
 
 // Middleware to handle 404 errors for all other routes
 Mealit.all('*', (req, res, next) => {
