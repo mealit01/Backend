522f2955cecdf13a9d63eecdb9484f12210c55fc
lamasalah32 <lamasalah32@gmail.com>
2023-06-18 01:25:12 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const Mealit = require('./Mealit');
+
+dotenv.config({ path: './config.env' });
+
+const cors = require("cors");
 const DB =  process.env.DATABASE;
+
+Mealit.use(cors())
 
 mongoose
   .connect(DB, {
