7128e6f78a287e37db533cc098bc14fe4613d397
lamasalah32 <lamasalah32@gmail.com>
2023-06-19 00:14:41 +0300

    update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ const authController = require('./../controllers/authController');
 router.post(`/signup`, authController.signup);
 
 router.post(`/login`, authController.login);
+router.post(`/logout`,authController.protect, authController.logout);
 router.post(`/forgetPassword`, authController.forgetPass);
 router.patch(`/resetPassword/:token`, authController.resetPass);
 router.patch(
@@ -15,7 +16,7 @@ router.patch(
 );
 router.patch(`/updateInfo`, authController.protect, userController.update);
 router.delete(`/delete`, authController.protect, userController.delete);
-router.route(`/info`).get(authController.protect, userController.getName);
+router.route(`/info`).get(authController.protect, userController.getInfo);
 
 router.route(`/`).get(authController.protect, userController.getAllUsers);
 
