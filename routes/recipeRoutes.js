61700a54deb123c6013750d3d9b4e52b50bde45d
lamasalah32 <lamasalah32@gmail.com>
2023-06-29 16:44:20 +0300

    update recipeRoutes.js

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -4,9 +4,13 @@ const recipesController = require('../controllers/recipesController');
 
 const router = express.Router();
 
-router.route(`/`).get(authController.protect, recipesController.getAllRecipes);
+router
+  .route(`/`)
+  .get(authController.checkIfUserLoggedIn, recipesController.getAllRecipes);
 
-router.route(`/getRecipe/:id`).get(recipesController.getRecipeById);
+router
+  .route(`/getRecipe/:id`)
+  .get(authController.checkIfUserLoggedIn, recipesController.getRecipeById);
 router.patch(`/updateRecipe/:id`, recipesController.update);
 
 router.delete(`/delete/:id`, recipesController.delete);
