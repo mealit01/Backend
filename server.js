3030dc04211fb0c6aa9bbf7704fac99686d93720
lamasalah32 <lamasalah32@gmail.com>
2023-06-29 05:39:13 +0300

    update vercel.json server.js package.json

---
+++ server.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config({ path: './config.env' });
+
+const Mealit = require('./Mealit');
+
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection sucsessful!'));
+
+// Set the port
+const port = 4000 || process.env.PORT;
+
+// Start the Express server
+Mealit.listen(port, () => {
+  console.log(`app running on port: ${port}..`);
+});
