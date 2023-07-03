3f34f353df5afaaf7ae95df4ada68dc45347b40c
lamasalah32 <lamasalah32@gmail.com>
2023-07-03 17:26:00 +0300

    update searchController.js

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,27 +1,9 @@
+const fs = require('fs');
+
 exports.getSearchFilters = async (req, res, next) => {
-  const filters = [
-    'preparation_time',
-    'cooking_time',
-    'total_time',
-    'servings',
-    'calories',
-    'carbohydrates',
-    'sugars',
-    'fat',
-    'cholesterol',
-    'protein',
-    'dietary_fiber',
-    'sodium',
-    'calories_from_fat',
-    'calcium',
-    'iron',
-    'magnesium',
-    'potassium',
-    'vitamin_c',
-    'diet_type',
-    'allergens',
-    'cuisine',
-  ];
+  const filters = JSON.parse(
+    fs.readFileSync(`${__dirname}/../data/filter.json`)
+  );
 
   res.status(200).json({
     status: 'success',
