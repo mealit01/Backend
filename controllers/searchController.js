b5792ea69500275c843058de826c2ab3dfe780f9
lamasalah32 <lamasalah32@gmail.com>
2023-07-04 14:18:31 +0300

    update searchController.js

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,6 +1,11 @@
+const Recipes = require('../models/recipesModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const getRes = require('../utils/getRes');
+const getImageUrl = require('../utils/getImageUrl');
 const fs = require('fs');
 
-exports.getSearchFilters = async (req, res, next) => {
+exports.getSearchFilters = catchAsync(async (req, res, next) => {
   const filters = JSON.parse(
     fs.readFileSync(`${__dirname}/../data/filter.json`)
   );
@@ -10,4 +15,26 @@ exports.getSearchFilters = async (req, res, next) => {
     length: filters.length,
     filters,
   });
-};
+});
+
+exports.search = catchAsync(async (req, res, next) => {
+  const responseData = await getRes(req.body);
+
+  let data = [];
+  await Promise.all(
+    responseData.map(async (url) => {
+      const recipe = await Recipes.findOne({ url });
+      const imageUrl = await getImageUrl(recipe.url);
+      recipe.imageUrl = imageUrl;
+      await recipe.save();
+
+      data.push(recipe);
+    })
+  );
+
+  res.status(200).json({
+    status: 'success',
+    length: data.length,
+    data,
+  });
+});
