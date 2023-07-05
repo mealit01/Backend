faa26567a076c298c23083da97221aa4ac1d0c26
lamasalah32 <lamasalah32@gmail.com>
2023-07-06 01:07:23 +0300

    use HTTPS instead of HTTP

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const https = require('https');
+const fs = require('fs');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 
-const MealitApp  = require('./MealitApp');
+const MealitApp = require('./MealitApp');
 
 const DB = process.env.DATABASE;
 
@@ -15,10 +17,15 @@ mongoose
   })
   .then(() => console.log('DB connection sucsessful!'));
 
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+};
+
 // Set the port
 const port = 4000 || process.env.PORT;
 
 // Start the Express server
-MealitApp.listen(port, () => {
-  console.log(`app running on port: ${port}..`);
+https.createServer(options, MealitApp).listen(port, () => {
+  console.log(`App running on port: ${port} with SSL enabled.`);
 });
