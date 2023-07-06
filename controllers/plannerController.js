c26aad8d644d243aa9eee799972cb0010bb32ab4
lamasalah32 <lamasalah32@gmail.com>
2023-07-06 16:11:56 +0300

    add plannerController.js

---
+++ controllers/plannerController.js
@@ -0,0 +1,115 @@
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+const getDays = (user, startDate, endDate) => {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+  start.setDate(start.getDate() - 10);
+  end.setDate(end.getDate() + 10);
+
+  const days = [];
+  while (start <= end) {
+    const day = start.getDate();
+    const dayOfWeek = daysOfWeek[start.getDay()];
+
+    let plannerElement =
+      user.planner.find(
+        (el) => el.day === day * 1 && el.dayOfWeek === dayOfWeek
+      ) || {};
+
+    const { breakfast = [], lunch = [], dinner = [] } = plannerElement;
+
+    days.push({ day, dayOfWeek, breakfast, lunch, dinner });
+    start.setDate(start.getDate() + 1);
+  }
+
+  return days;
+};
+
+exports.getPlannerDays = catchAsync(async (req, res) => {
+  const { _id } = req.user;
+  const days = getDays(req.user, new Date(), new Date());
+
+  const updatedUser = await User.findByIdAndUpdate(
+    _id,
+    { planner: days },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  res.status(200).json({
+    status: 'success',
+    days: updatedUser.planner,
+  });
+});
+
+exports.getDay = catchAsync(async (req, res) => {
+  let day = req.user.planner.find((el) => el.day === req.params.day * 1);
+
+  if (!day) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Day not found',
+    });
+  }
+
+  day = await User.findOne({ _id: req.user._id })
+    .populate('planner.breakfast planner.lunch planner.dinner')
+    .exec();
+
+  console.log(day);
+
+  res.status(200).json({
+    status: 'success',
+    day: day.planner.find((el) => el.day === req.params.day * 1),
+  });
+});
+
+exports.addMeal = catchAsync(async (req, res) => {
+  const { meal, day, id } = req.params;
+  console.log(meal, day, id);
+
+  const planner = req.user.planner.find((el) => el.day === Number(day));
+  planner[meal].addToSet(id);
+
+  const updatedUser = await User.findByIdAndUpdate(req.user._id, req.user, {
+    new: true,
+    runValidators: true,
+  });
+
+  const updatedPlanner = updatedUser.planner.find(
+    (el) => el.day === Number(day)
+  );
+
+  res.status(200).json({
+    status: 'success',
+    day: updatedPlanner,
+  });
+});
+
+exports.deleteMeal = catchAsync(async (req, res) => {
+  const { meal, day, id } = req.params;
+  console.log(meal, day, id);
+
+  const planner = req.user.planner.find((el) => el.day === Number(day));
+  planner[meal].pull(id);
+
+  const updatedUser = await User.findByIdAndUpdate(req.user._id, req.user, {
+    new: true,
+    runValidators: true,
+  });
+
+  const updatedPlanner = updatedUser.planner.find(
+    (el) => el.day === Number(day)
+  );
+
+  res.status(200).json({
+    status: 'success',
+    day: updatedPlanner,
+  });
+});
